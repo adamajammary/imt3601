930c79b213019a7f1529d4e640b36312c3c25e57
Michael Bråten <braaten.michael@outlook.com>
2017-09-12 17:33:48 +0200

    Only the bunny can shoot poop now

--- BunnyGame/Assets/Scripts/SetUpLocalPlayer.cs
+++ BunnyGame/Assets/Scripts/SetUpLocalPlayer.cs
@@ -5,8 +5,10 @@ using UnityEngine.Networking;
 
 public class SetUpLocalPlayer : NetworkBehaviour {
     NetworkTransformChild modelNetworkTransform;
-	// Use this for initialization
-	void Start () {
+
+
+    // Use this for initialization
+    void Start () {
         if (this.isLocalPlayer) {
             this.gameObject.AddComponent<PlayerController>();
             ThirdPersonCamera camera = FindObjectOfType<ThirdPersonCamera>();
@@ -16,15 +18,16 @@ public class SetUpLocalPlayer : NetworkBehaviour {
             this.tag = "Enemy";
 
         setupClass();
-
     }
 
     void setupClass() {
 
         // Temporarily choosing class at random
-        // NB! This random class isn't synced across clients...
-        int r = Random.Range(0, 2);
-        switch (r) {
+        // NB! This random choice isn't synced across clients
+
+        int animal = Random.Range(0,2);
+
+        switch (animal) {
             case 0:
                 setupFox();
                 break;
@@ -34,21 +37,23 @@ public class SetUpLocalPlayer : NetworkBehaviour {
         }
     }
 
-    private void loadModel(string name) {
+    // Load a model for the character from a prefab
+    // Prefabs for this has to be stored in "Assets/Resources/Prefabs/"
+    private void loadAnimalModel(string name) {
         GameObject prefab = Resources.Load<GameObject>("Prefabs/"+name);
         GameObject model = Instantiate(prefab);
         model.transform.SetParent(transform);
         model.transform.localPosition = Vector3.zero;
         model.transform.rotation = transform.rotation;
-   
     }
 
     // Setup the player as a Fox
     private void setupFox() {
-        loadModel("FoxModel");
+        loadAnimalModel("FoxModel");
 
         if (!this.isLocalPlayer)
             return;
+
         // Setup fox-specific attributes, abilities, etc...
         PlayerController pc = GetComponent<PlayerController>();
         pc.runSpeed = 15;
@@ -58,11 +63,14 @@ public class SetUpLocalPlayer : NetworkBehaviour {
 
     // Setup the player as a bunny
     private void setupBunny() {
-        loadModel("BunnyModel");
+        loadAnimalModel("BunnyModel");
 
         if (!this.isLocalPlayer)
             return;
+
         // Set up bunny-specific attributes, abilities, etc...
+        BunnyCommands commands = gameObject.AddComponent<BunnyCommands>();
+        commands.bunnyPoop = Resources.Load<GameObject>("Prefabs/poop");
         PlayerController pc = GetComponent<PlayerController>();
         pc.jumpHeight = 3;
     }

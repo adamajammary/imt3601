0c7824449367e49c33edbc103110cca8666d3f45
Michael Bråten <braaten.michael@outlook.com>
2017-09-12 15:59:15 +0200

    Fixed bug where players couldn't see eachothers models.

--- BunnyGame/Assets/Scripts/SetUpLocalPlayer.cs
+++ BunnyGame/Assets/Scripts/SetUpLocalPlayer.cs
@@ -4,12 +4,9 @@ using UnityEngine;
 using UnityEngine.Networking;
 
 public class SetUpLocalPlayer : NetworkBehaviour {
-
+    NetworkTransformChild modelNetworkTransform;
 	// Use this for initialization
 	void Start () {
-        setupModel();
-
-
         if (this.isLocalPlayer) {
             this.gameObject.AddComponent<PlayerController>();
             ThirdPersonCamera camera = FindObjectOfType<ThirdPersonCamera>();
@@ -17,11 +14,57 @@ public class SetUpLocalPlayer : NetworkBehaviour {
             this.tag = "Player";
         } else
             this.tag = "Enemy";
-	}
 
-    void setupModel() {
-        GameObject model = Instantiate(Resources.Load<GameObject>("Prefabs/BunnyModel"));
-        model.transform.SetParent(transform);
+        setupClass();
+
     }
-	
+
+    void setupClass() {
+
+        // Temporarily choosing class at random
+        // NB! This random class isn't synced across clients...
+        int r = Random.Range(0, 2);
+        switch (r) {
+            case 0:
+                setupFox();
+                break;
+            case 1:
+                setupBunny();
+                break;
+        }
+    }
+
+    private void loadModel(string name) {
+        GameObject prefab = Resources.Load<GameObject>("Prefabs/"+name);
+        GameObject model = Instantiate(prefab);
+        model.transform.SetParent(transform);
+        model.transform.localPosition = Vector3.zero;
+        model.transform.rotation = transform.rotation;
+   
+    }
+
+    // Setup the player as a Fox
+    private void setupFox() {
+        loadModel("FoxModel");
+
+        if (!this.isLocalPlayer)
+            return;
+        // Setup fox-specific attributes, abilities, etc...
+        PlayerController pc = GetComponent<PlayerController>();
+        pc.runSpeed = 15;
+
+
+    }
+
+    // Setup the player as a bunny
+    private void setupBunny() {
+        loadModel("BunnyModel");
+
+        if (!this.isLocalPlayer)
+            return;
+        // Set up bunny-specific attributes, abilities, etc...
+        PlayerController pc = GetComponent<PlayerController>();
+        pc.jumpHeight = 3;
+    }
+
 }

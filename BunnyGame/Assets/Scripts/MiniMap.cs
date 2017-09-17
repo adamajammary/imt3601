f320e8e689d09af0d519276e35bc763ea88e55da
Michael Bråten <braaten.michael@outlook.com>
2017-09-17 14:32:10 +0200

    Allow player to switch minimap mode with M-key

--- BunnyGame/Assets/Scripts/MiniMap.cs
+++ BunnyGame/Assets/Scripts/MiniMap.cs
@@ -5,9 +5,8 @@ using UnityEngine;
 
 public class MiniMap : MonoBehaviour {
 
-    public MinimapMode mode = MinimapMode.VIEW_ALL;
-
     private GameObject _player;
+    private MinimapMode _mode;
     private int _cameraHeight = 300;
 
     public enum MinimapMode {
@@ -17,21 +16,33 @@ public class MiniMap : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+        setCameraMode(MinimapMode.VIEW_ALL);
+	}
+	
 
+	void Update () {
+        if(_mode == MinimapMode.FOLLOW_PLAYER)
+            transform.position = new Vector3(_player.transform.position.x, _cameraHeight, _player.transform.position.z);
+
+        if (Input.GetKeyDown(KeyCode.M)) {
+            if (_mode == MinimapMode.FOLLOW_PLAYER)
+                setCameraMode(MinimapMode.VIEW_ALL);
+            else if (_mode == MinimapMode.VIEW_ALL)
+                setCameraMode(MinimapMode.FOLLOW_PLAYER);
+        }
+	}
+
+    void setCameraMode(MinimapMode mode) {
+        _mode = mode;
         switch (mode) {
             case MinimapMode.FOLLOW_PLAYER:
                 _player = GameObject.FindGameObjectWithTag("Player");
+                GetComponent<Camera>().orthographicSize = 65;
                 break;
             case MinimapMode.VIEW_ALL:
                 transform.position = new Vector3(10, _cameraHeight, 5);
                 GetComponent<Camera>().orthographicSize = 200;
                 break;
         }
-	}
-	
-
-	void Update () {
-        if(mode == MinimapMode.FOLLOW_PLAYER)
-            transform.position = new Vector3(_player.transform.position.x, _cameraHeight, _player.transform.position.z);
-	}
+    }
 }

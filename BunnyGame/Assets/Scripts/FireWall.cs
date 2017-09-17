9ffba789cf2e454d1e56791ba00b3ca232b600e4
Muff1nz <muffinz314@gmail.com>
2017-09-17 22:58:39 +0200

    Wall now halves in size every x seconds, wont implement damage until we have a health system

--- BunnyGame/Assets/Scripts/FireWall.cs
+++ BunnyGame/Assets/Scripts/FireWall.cs
@@ -6,12 +6,13 @@ public class FireWall : MonoBehaviour {
     private Material _fs;
     private float _noiseSeed = 0;
     private float _noiseSpeed = 1.25f;
+    private float _wallShrinkTime = 10.0f; //Time in seconds between _wall shrinking
+    private float _wallShrinkTimer = 0.0f; //Timer for when to shrink _wall
 
-    Mesh wall;
-    float t = 0;
-
-    Circle start;
-    Circle target;
+    private Mesh _wall;
+    private Circle _current;
+    private Circle _target;
+    private float _targetRadius = 250;
 
 	// Use this for initialization
 	void Start () {
@@ -19,14 +20,39 @@ public class FireWall : MonoBehaviour {
         this._ft = new Texture2D(128, 128, TextureFormat.ARGB32, false);
         this._fs.mainTexture = this._ft;
 
-        start = new Circle(250, Vector3.zero);
-        target = new Circle(100, new Vector3(50, 0, 50));
+        this._current = new Circle(250, Vector3.zero);
+        this._target = new Circle(250, Vector3.zero);
    
-        wall = GetComponent<Mesh>();
+        this._wall = GetComponent<Mesh>();
+
+        
     }
 	
 	// Update is called once per frame
 	void Update () {
+        this.generateWallTexture();
+
+        if (this._wallShrinkTimer > this._wallShrinkTime) {
+            this.recalculateWalls();
+            StartCoroutine(interpolateWall());
+            this._wallShrinkTimer = 0;
+        }
+        this._wallShrinkTimer += Time.deltaTime;
+
+        Debug.Log(string.Format("Wall will shrink in : {0}", this._wallShrinkTime - this._wallShrinkTimer));
+    }
+
+    private void recalculateWalls() {
+        this._current = this._target;
+        this._targetRadius /= 2.0f;
+
+        float angle = Random.Range(0, 1) * Mathf.PI * 2;
+        float currentWallOffset = Random.Range(0, this._current._radius - this._targetRadius);
+        Vector3 targetPos = this._current._pos + new Vector3(Mathf.Cos(angle), 0, Mathf.Sin(angle)) * currentWallOffset;
+        this._target = new Circle(this._targetRadius, targetPos);
+    }
+
+    private void generateWallTexture() {
         //Sets every pixel in the firewall texture
         for (int y = 0; y < _ft.height; y++) {
             for (int x = 0; x < _ft.width; x++) {
@@ -35,21 +61,21 @@ public class FireWall : MonoBehaviour {
             }
         }
         // Apply all pixel changes to the texture
-        this._ft.Apply();        
+        this._ft.Apply();
         // This will change where noise is sampled from the noise plane
         this._noiseSeed += this._noiseSpeed * Time.deltaTime;
-
-        interpolateWall();  
     }
 
-    void interpolateWall() {
-        if (t >= 1)
-            return;
+    private IEnumerator<bool> interpolateWall() {
+        float t = 0;
 
-        transform.position = Vector3.Lerp(start.getWall().transform.position, target.getWall().transform.position, t);
-        transform.localScale = Vector3.Lerp(start.getWall().transform.localScale, target.getWall().transform.localScale, t);
+        while (t <= 1) {
+            transform.position = Vector3.Lerp(_current._wall.transform.position, _target._wall.transform.position, t);
+            transform.localScale = Vector3.Lerp(_current._wall.transform.localScale, _target._wall.transform.localScale, t);
 
-        t += 0.1f * Time.deltaTime;
+            t += 0.3f * Time.deltaTime;
+            yield return false;
+        }
     }
 
     // Generates values from 0.4-1.0 based on perlin noise
@@ -66,21 +92,18 @@ public class FireWall : MonoBehaviour {
 }
 
 class Circle {
-    private float _radius;
-    private Vector3 _pos;
-    private GameObject _wall;
+    public float _radius;
+    public Vector3 _pos;
+    public GameObject _wall;
 
     public Circle(float radius, Vector3 pos) {
         this._radius = radius;
         this._pos = pos;
 
-        this._wall = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+        this._wall = Resources.Load<GameObject>("Prefabs/WallShell");
+        this._wall = MonoBehaviour.Instantiate(this._wall);
         this._wall.transform.position = pos;
         this._wall.transform.localScale = new Vector3(this._radius * 2, 500, this._radius * 2);
-    }
-
-    public GameObject getWall() {
-        return this._wall;
     }
 }
 

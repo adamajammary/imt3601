a084c767d9bf8ecc53df4d71108a3ee28c2ca742
Michael Bråten <braaten.michael@outlook.com>
2017-09-19 19:35:09 +0200

    Fox can now bite to deal damage

--- BunnyGame/Assets/Scripts/Fox/FoxController.cs
+++ BunnyGame/Assets/Scripts/Fox/FoxController.cs
@@ -3,26 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 
+
 public class FoxController : NetworkBehaviour {
     GameObject biteArea;
+
+    private int _biteDamage = 15;
 
     // Use this for initialization
     void Start()
     {
+        biteArea = transform.GetChild(2).gameObject;
+
         if (!this.isLocalPlayer)
             return; 
 
         PlayerController playerController = GetComponent<PlayerController>();
         playerController.runSpeed = 15;
 
-        biteArea = Instantiate(GameObject.CreatePrimitive(PrimitiveType.Cube));
-        biteArea.transform.SetParent(transform);
-        biteArea.transform.localPosition = new Vector3(0, 0.03f, 0.34f);
-        biteArea.name = "FoxBiteHitbox";
-        biteArea.tag = "projectile"; // Temporarily, should preferrably have its own tag so that it can deal a different amout of damage than bunnypoop. (see PlayerController.OnCollisionEnter()
-        biteArea.GetComponent<BoxCollider>().enabled = false;
-        biteArea.transform.localScale = Vector3.zero;
-        NetworkServer.Spawn(biteArea);
     }
 
     // Update is called once per frame
@@ -32,17 +29,24 @@ public class FoxController : NetworkBehaviour {
             return;
 
         if (Input.GetKeyDown(KeyCode.Mouse0)) {
-            StartCoroutine(this.bite());
+            CmdBite();
         }
     }
 
     // Biting is enabled for 1 tick after called
     private IEnumerator bite()
     {
-        biteArea.GetComponent<BoxCollider>().enabled = true;
-        biteArea.transform.localScale = new Vector3(0.6f, 0.6f, 0.6f);
+        biteArea.GetComponent<BoxCollider>().enabled = true; 
         yield return 0;
         biteArea.GetComponent<BoxCollider>().enabled = false;
-        biteArea.transform.localScale = Vector3.zero;
+    }
+    [Command]
+    private void CmdBite()
+    {
+        StartCoroutine(bite());
+    }
+
+    public int getDamage() {
+        return _biteDamage;
     }
 }

605057968e0d560b8019445f5a89a03d15569140
Michael Bråten <braaten.michael@outlook.com>
2017-09-20 14:59:10 +0200

    Player can't take damage for the first 5 seconds joining the server

--- BunnyGame/Assets/Scripts/PlayerHealth.cs
+++ BunnyGame/Assets/Scripts/PlayerHealth.cs
@@ -1,13 +1,20 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.Networking;
 using UnityEngine.UI;
 
 public class PlayerHealth : NetworkBehaviour {
 
     private const int MAX_HEALTH = 100;
+    private bool _damageImmune = true;
 
     [SyncVar(hook = "OnChangeHealth")]
     private int _currentHealth = MAX_HEALTH;
+
+    void Start() {
+        StartCoroutine(damageImmune(5)); // Make player immune from damage for the first 5 seconds after spawning
+    }
 
     // Update the health/damage screen overlay on the client whenever the health value changes on the server
     private void OnChangeHealth(int health) {
@@ -22,7 +29,7 @@ public class PlayerHealth : NetworkBehaviour {
 
     // Take damage when hit, and respawn the player if dead.
     public void TakeDamage(int amount) {
-        if (!this.isServer) { return; }
+        if (!this.isServer || _damageImmune) { return; }
 
         this._currentHealth -= amount;
         
@@ -33,6 +40,13 @@ public class PlayerHealth : NetworkBehaviour {
     }
     public void TakeDamage(float amount) {
         TakeDamage((int)amount);
+    }
+
+    // Make the player immune from damage for <seconds>
+    private IEnumerator damageImmune(float seconds) {
+        _damageImmune = true;
+        yield return new WaitForSeconds(seconds);
+        _damageImmune = false;
     }
 
 

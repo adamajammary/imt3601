2337b5d565f92726915664d25b1a17a855e5686b
magweng <magnus.enggrav@gmail.com>
2017-09-28 11:11:19 +0200

    new Stealth.cs uses SpecialAbility new AbilityNetwork.cs for Abilities that requires networking

--- BunnyGame/Assets/Scripts/Fox/FoxController.cs
+++ BunnyGame/Assets/Scripts/Fox/FoxController.cs
@@ -6,29 +6,18 @@ using UnityEngine.Networking;
 
 public class FoxController : NetworkBehaviour {
     GameObject biteArea;
-    GameObject foxModel;
-
-    Material[] objMaterials;
-    Color alfaColor;
-
+  
     private PlayerHealth _playerHealth;
 
     private int   _biteDamage       = 15;
-    private float _cooldownStealth  = 30.0f;
-    private float _stealthActive    = 10.0f;
-    private float _transparency     = 0.1f;
-    private float _notTransparent   = 1.0f;
-    private float _stealthTime      = 31.0f;
 
     // Use this for initialization
-
     void Start()
     {
         if (!this.isLocalPlayer)
             return;
 
         this._playerHealth = this.GetComponent<PlayerHealth>();
-        foxModel = transform.GetChild(1).gameObject;
        
         biteArea = transform.GetChild(2).gameObject;
 
@@ -40,11 +29,13 @@ public class FoxController : NetworkBehaviour {
         Sprint sp = gameObject.AddComponent<Sprint>();
         sp.init(50, 1);
         playerController.abilities.Add(sp);
+        Stealth st = gameObject.AddComponent<Stealth>();
+        st.init(1, 0.1f);
+        playerController.abilities.Add(st);
         GameObject.Find("AbilityPanel").GetComponent<AbilityPanel>().setupPanel(playerController);
     }
 
     // Update is called once per frame
-
     void Update() {
         if (!this.isLocalPlayer)
             return;
@@ -52,20 +43,9 @@ public class FoxController : NetworkBehaviour {
         if (Input.GetKeyDown(KeyCode.Mouse0)) {
             CmdBite();
         }
-
-        this._stealthTime += Time.deltaTime;
-        //The '1' key on the top of the alphanumeric keyboard
-        if (Input.GetKeyDown(KeyCode.Alpha2)) {
-            if (this._stealthTime >= this._cooldownStealth)
-            {
-                CmdStealth();
-                this._stealthTime = 0;
-            }     
-        }   
     }
 
     // Biting is enabled for 1 tick after called
-
     private IEnumerator bite() {
         biteArea.GetComponent<BoxCollider>().enabled = true; 
         yield return 0;
@@ -81,50 +61,5 @@ public class FoxController : NetworkBehaviour {
 
     public int getDamage() {
         return _biteDamage;
-    }
-
-    [Command]
-    private void CmdStealth()
-    {
-        StartCoroutine(stealth());
-    }
-
-    private IEnumerator stealth()
-    {
-        RpcSetTransparentFox();
-        yield return new WaitForSeconds(this._stealthActive);
-        RpcSetOrginalFox();
-    }
-
-    [ClientRpc]
-    private void RpcSetTransparentFox()
-    {
-        foreach (Transform child in foxModel.transform)
-        {
-            objMaterials = child.gameObject.GetComponent<Renderer>().materials;
-            int count = 0;
-            foreach (Material mat in objMaterials)
-            {
-                alfaColor = mat.color;
-                alfaColor.a = _transparency;
-                objMaterials[count++].SetColor("_Color", alfaColor);
-            }
-        }
-    }
-
-    [ClientRpc]
-    private void RpcSetOrginalFox()
-    {
-        foreach (Transform child in foxModel.transform)
-        {
-            objMaterials = child.gameObject.GetComponent<Renderer>().materials;
-            int count = 0;
-            foreach (Material mat in objMaterials)
-            {
-                alfaColor = mat.color;
-                alfaColor.a = _notTransparent;
-                objMaterials[count++].SetColor("_Color", alfaColor);
-            }
-        }
     }
 }

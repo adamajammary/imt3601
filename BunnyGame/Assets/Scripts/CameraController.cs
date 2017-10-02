09091fd1db68799328f414e5d42dfebf0d27a08c
besari <31652673+besari@users.noreply.github.com>
2017-10-02 11:42:36 +0200

    Move the spectating camera with mouse cursor

--- BunnyGame/Assets/Scripts/CameraController.cs
+++ BunnyGame/Assets/Scripts/CameraController.cs
@@ -3,6 +3,7 @@
 public class CameraController : MonoBehaviour {
 
 	public float panSpeed = 20f;
+	public float panBorderThickness = 10f;
 	public Vector2 panLimit;
 	public float scrollSpeed = 20f;
 	public float minY = 20f;
@@ -13,22 +14,22 @@ public class CameraController : MonoBehaviour {
 
 		Vector3 pos = transform.position;
 
-		if (Input.GetKey ("w")) {
+		if (Input.GetKey ("w") || Input.mousePosition.y >= Screen.height - panBorderThickness) {
 
 			pos.z += panSpeed * Time.deltaTime;
 		}
 
-		if (Input.GetKey ("s")) {
+		if (Input.GetKey ("s") || Input.mousePosition.y <= panBorderThickness) {
 
 			pos.z -= panSpeed * Time.deltaTime;
 		}
 
-		if (Input.GetKey ("d")) {
+		if (Input.GetKey ("d") || Input.mousePosition.x >= Screen.width - panBorderThickness) {
 
 			pos.x += panSpeed * Time.deltaTime;
 		}
 
-		if (Input.GetKey ("a")) {
+		if (Input.GetKey ("a") || Input.mousePosition.x <= panBorderThickness ) {
 
 			pos.x -= panSpeed * Time.deltaTime;
 		}

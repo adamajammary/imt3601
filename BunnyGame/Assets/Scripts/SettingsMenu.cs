de57f9fae9e6ed27038de54103358de718db9d09
Michael Bråten <braaten.michael@outlook.com>
2017-10-07 18:10:47 +0200

    started work on a settings UI

--- BunnyGame/Assets/Scripts/SettingsMenu.cs
+++ BunnyGame/Assets/Scripts/SettingsMenu.cs
@@ -5,9 +5,11 @@ using UnityEngine.UI;
 
 public class SettingsMenu : MonoBehaviour {
 	// Use this for initialization
+
 	void Start () {
         createSettings();
-	}
+        close();
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -15,42 +17,125 @@ public class SettingsMenu : MonoBehaviour {
 	}
 
 
-    public void createSettings() {
-        GameObject panel = transform.GetChild(0).gameObject;
-
-        GameObject nameSection = addSection("Default name", panel);
-        GameObject customDefaultName = addTextOption("", nameSection);
-
-        GameObject soundSection = addSection("Sound", panel);
-        GameObject masterVolume = addSliderOption("Master Volume", soundSection, 0, 100);
-        GameObject musicVolume = addSliderOption("Music Volume", soundSection, 0, 100);
-
-        GameObject cameraSection = addSection("Camera", panel);
-        GameObject fov = addSliderOption("FOV", cameraSection, 50, 150);
-        GameObject mouseSensitivity = addSliderOption("Mouse Sensitivity", cameraSection, 0, 100);
-
-        GameObject videoSettings = addSection("Video", panel);
-        GameObject resolution = addDropdownOption("Resolution", panel, new string[]{"1920x1080","1280x720"});
+    public void open() {
+        transform.GetChild(0).gameObject.SetActive(true);
+    }
+    public void close() {
+        transform.GetChild(0).gameObject.SetActive(false);
+    }
 
 
+    private void createSettings() {
+        GameObject panel = transform.GetChild(0).GetChild(0).GetChild(0).GetChild(0).gameObject; // Yep
 
+        GameObject testSection = addSection("Test section", panel);
+        GameObject textoptionTest = addTextOption("option name", testSection, "placeholder text");
+
+
+        //GameObject nameSection = addSection("Default name", panel);
+        //GameObject customDefaultName = addTextOption("", nameSection);
+
+        //GameObject soundSection = addSection("Sound", panel);
+        //GameObject masterVolume = addSliderOption("Master Volume", soundSection, 0, 100);
+        //GameObject musicVolume = addSliderOption("Music Volume", soundSection, 0, 100);
+
+        //GameObject cameraSection = addSection("Camera", panel);
+        //GameObject fov = addSliderOption("FOV", cameraSection, 50, 150);
+        //GameObject mouseSensitivity = addSliderOption("Mouse Sensitivity", cameraSection, 0, 100);
+
+        //GameObject videoSettings = addSection("Video", panel);
+        //GameObject resolution = addDropdownOption("Resolution", panel, new string[]{"1920x1080","1280x720"});
+
+
+        // Add save and cancel button
 
         pack(panel);
     }
 
-    public GameObject addSection(string title, GameObject parent) {
-        GameObject sectionPanel = Resources.Load<GameObject>("Prefabs/Settings/Section");
+    // Creates a basic ui object with a rect transform and a canvas renderer
+    private GameObject createBaseUIObject(string objectName = "Unnamed", GameObject parent = null)
+    {
+        GameObject uiObject = new GameObject();
+        uiObject.AddComponent<RectTransform>();
+        uiObject.AddComponent<CanvasRenderer>();
+        uiObject.name = objectName;
+        if(parent != null)
+            uiObject.transform.SetParent(parent.transform);
 
-        sectionPanel.transform.SetParent(parent.transform);
+        return uiObject;
+    }
+
+    private GameObject addSection(string title, GameObject parent) {
+        GameObject sectionPanel = createBaseUIObject("Section: " + title, parent);
+
+        // Set section width, background etc..
+        // Height will be set during packing
+
+        GameObject sectionTitle = createBaseUIObject("Section title: " + title, sectionPanel);
+        sectionTitle.AddComponent<Text>().text = title;
+        // Set title font size, color, positioning, etc...
+
+
         return sectionPanel;
     }
 
+    private GameObject addBasicOption(string text, GameObject parent) {
+        GameObject option = createBaseUIObject("Option panel: " + text, parent);
+        // Set up size, position, etc...
+
+        GameObject optionText = createBaseUIObject("Option Text: " + text, option);
+        optionText.AddComponent<Text>().text = text;
+        // Set up size, position, font size, font color, etc...
+
+        GameObject interactiveElement = createBaseUIObject("Option: " + text, option);
+        // Set up size and position etc...
+
+
+        return interactiveElement;
+    }
+
+
+    private GameObject addTextOption(string optionName, GameObject parent, string placeholderText = "") {
+        GameObject option = addBasicOption(optionName, parent);
+        option.AddComponent<Image>();
+        option.AddComponent<InputField>();
+
+        GameObject text = createBaseUIObject("Text: " + optionName, option);
+        text.AddComponent<Text>();
+
+        GameObject placeholder = createBaseUIObject("Placeholder: " + optionName, option);
+        placeholder.AddComponent<Text>();
+
+        return option;
+    }
+
+    private GameObject addDropdownOption(string optionName, GameObject parent, string[] elements) {
+        GameObject option = addBasicOption(optionName, parent);
+
+        return option;
+    }
+
+    private GameObject addSliderOption(string optionName, GameObject parent, int minval, int maxval) {
+        GameObject option = addBasicOption(optionName, parent);
+
+        return option;
+    }
+
+
+    private GameObject addToggleOption(string optionName, GameObject parent) {
+        GameObject option = addBasicOption(optionName, parent);
+
+        return option;
+    }
+
+
     // Resize the panel and sections inside so that they fit the number of settings and size of the window
-    private void pack(GameObject panel) {
+    private void pack(GameObject panel)
+    {
         // Starting at 1 becuase child 0 is the Settings window title
-        for(int i = 1; i < panel.transform.childCount; i++) {
+        for (int i = 1; i < panel.transform.childCount; i++) {
             int sectionItems = panel.transform.GetChild(i).childCount - 1; // Child 1 is section title
-            
+
             // TODO set the size of the panel based on how many children it has
 
         }
@@ -59,62 +144,5 @@ public class SettingsMenu : MonoBehaviour {
 
     }
 
-    // Creates a basic ui object with a rect transform and a canvas renderer
-    private GameObject createBaseUIObject()
-    {
-        GameObject uiObject = new GameObject();
-        uiObject.AddComponent<RectTransform>();
-        uiObject.AddComponent<CanvasRenderer>();
-        return uiObject;
-    }
 
-    private GameObject addBasicOption(string text, GameObject parent) {
-        GameObject option = createBaseUIObject();
-        // Set up size, position, etc...
-
-        GameObject optionText = createBaseUIObject();
-        optionText.transform.SetParent(option.transform);
-        optionText.AddComponent<Text>();
-        optionText.GetComponent<Text>().text = text;
-        // Set up size, position, font size, font color, etc...
-
-        GameObject interactiveElement = createBaseUIObject();
-        interactiveElement.transform.SetParent(option.transform);
-        // Set up size and position etc...
-
-
-        return interactiveElement;
-    }
-
-    public GameObject addDropdownOption(string name, GameObject parent, string[] elements) {
-        GameObject optionpanel = addBasicOption(name, parent);
-
-
-        GameObject dropdown = Resources.Load<GameObject>("Prefabs/Settings/Dropdown");
-        dropdown.transform.SetParent(parent.transform);
-        dropdown.GetComponent<Dropdown>();
-
-        return dropdown;
-    }
-
-    public GameObject addTextOption(string name, GameObject parent, string placeholderText = "") {
-        GameObject textOption = Resources.Load<GameObject>("Prefabs/Settings/TextOption");
-        textOption.transform.SetParent(parent.transform);
-
-        return textOption;
-    }
-
-    public GameObject addSliderOption(string name, GameObject parent, int minval, int maxval) {
-        GameObject sliderOption = Resources.Load<GameObject>("Prefabs/Settings/Slider");
-        sliderOption.transform.SetParent(parent.transform);
-
-        return sliderOption;
-    }
-
-    public GameObject addToggleOption(string name, GameObject parent) {
-        GameObject toggle = Resources.Load<GameObject>("Prefabs/Settings/Toggle");
-        toggle.transform.SetParent(parent.transform);
-
-        return toggle;
-    }
 }

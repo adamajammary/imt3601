d978e385037b0f8cb8bd8c1a702fa292015fdfe2
Michael Bråten <braaten.michael@outlook.com>
2017-10-20 22:04:22 +0200

    Added audio file for fire and tweaked the audio levels

--- BunnyGame/Assets/Scripts/AudioManager.cs
+++ BunnyGame/Assets/Scripts/AudioManager.cs
@@ -26,29 +26,25 @@ public class AudioManager : MonoBehaviour {
 
     // Music
     private AudioSource _music;
-    private AudioClip[] _musicPieces;
+    private AudioClip[] _musicClips;
 
 
 	void Start () {
-        StartCoroutine(musicController());
+        StartCoroutine(musicPlayer());
 
         updateVolume();
 
         // OCEAN:
-        GameObject oceanObj = new GameObject {
-            name = "OceanSound"
-        };
+        GameObject oceanObj = new GameObject {name = "OceanSound"};
         _ocean = oceanObj.AddComponent<AudioSource>();
         _ocean.clip = Resources.Load<AudioClip>("Audio/water_lapping_sea_waves_sandy_beach_5_meters_01");
         _ocean.Play();
         _ocean.loop = true;
 
         // FIRE:
-        GameObject fireObj = new GameObject {
-            name = "FireSound"
-        };
+        GameObject fireObj = new GameObject {name = "FireSound"};
         _fire = fireObj.AddComponent<AudioSource>();
-        //_fire.clip = Resources.Load<AudioClip>("Audio/...");
+        _fire.clip = Resources.Load<AudioClip>("Audio/nature_fire_big");
         _fire.Play();
         _fire.loop = true;
 
@@ -62,11 +58,11 @@ public class AudioManager : MonoBehaviour {
 
 	}
 
-
+    // Sets the volume of the ocean sound based on distance from the ocean
+    // also distorts sound when underwater
     private void updateOceanSound() {
-        float distFromCenter = Vector2.Distance(
-            new Vector2(this.cameraTransform.position.x, this.cameraTransform.position.z), 
-            Vector2.zero);
+        float distFromCenter = Vector2.Distance(new Vector2(this.cameraTransform.position.x, this.cameraTransform.position.z), 
+                                                Vector2.zero);
         float distFromOcean = mapRadius - distFromCenter;
 
         _ocean.volume = (Mathf.Max((distFromOcean > 0 ? 3f / distFromOcean : 1f), .05f) / 2f) * effectVolume;
@@ -74,52 +70,49 @@ public class AudioManager : MonoBehaviour {
 
 
         // TODO : Surround/Stereo?
-        // _ocean.panStereo = ...
     }
 
+
+    // Updates the volume of the fire sound based on distance from the firewall
+    // also distorts sound when underwater
     private void updateFireSound() {
         // No updating if wall hasn't been created yet
         if (_firewall == null) {
             GameObject wall = GameObject.FindGameObjectWithTag("FireWall");
-            if (wall == null)
-                return;
-            else
-                _firewall = wall.GetComponent<FireWall>();
+            if (wall == null) return;
+            else _firewall = wall.GetComponent<FireWall>();
         }
 
 
-        Vector2 firewallcenter = new Vector2(_firewall.transform.position.x, _firewall.transform.position.z);
+        float distFromCenter = Vector2.Distance(new Vector2(cameraTransform.position.x, cameraTransform.position.z),
+                                                new Vector2(_firewall.transform.position.x, _firewall.transform.position.z));
 
-        float distFromCenter = Vector2.Distance(
-            new Vector2(cameraTransform.position.x, cameraTransform.position.z),
-            firewallcenter);
-
-        float distFromWall = Mathf.Abs(_firewall.transform.localScale.x/2 - distFromCenter);
-
-        _fire.volume = (Mathf.Max((distFromWall > 0 ? 0.5f / distFromWall : 0.5f), .0005f) / 2f) * effectVolume;
-        _fire.pitch = _isUnderWater ? 0.2f : 1f;
+        float distFromWall = Mathf.Abs(_firewall.transform.localScale.x / 2 - distFromCenter);
+        
+        _fire.volume = (distFromWall > 0 ? 0.05f + 0.95f * Mathf.Pow(1 - distFromWall / 250, 4) : 1) / 2 * effectVolume;
+        //_fire.pitch = _isUnderWater ? 0.2f : 1f;
+        Debug.Log(_fire.volume + " :: " + distFromWall);
 
         // TODO : Surround/Stereo?
-        // _fire.panStereo = ...
     }
 
+
     // Plays music tracks in a loop
-    private IEnumerator musicController() {
-        _musicPieces = Resources.LoadAll<AudioClip>("Audio/Music/");
-        GameObject musicObj = new GameObject() {
-            name = "Music"
-        };
+    private IEnumerator musicPlayer() {
+        _musicClips = Resources.LoadAll<AudioClip>("Audio/Music/");
+        GameObject musicObj = new GameObject() {name = "Music"};
         _music = musicObj.AddComponent<AudioSource>();
 
-        updateVolume();
+        yield return null;
 
-        int clipIndex = Random.Range(0, _musicPieces.Length);
+        // TODO: Make the music clips fade in/out with overlap to make the transition between the tracks smoother
+
+        int clipIndex = Random.Range(0, _musicClips.Length);
         while (true) {
-            _music.PlayOneShot(_musicPieces[clipIndex]);
-            Debug.Log("Playing " + _musicPieces[clipIndex].name);
-            yield return new WaitForSeconds(_musicPieces[clipIndex].length);
-            clipIndex += 1;
-            clipIndex %= _musicPieces.Length;
+            _music.PlayOneShot(_musicClips[clipIndex]);
+            Debug.Log("Playing " + _musicClips[clipIndex].name);
+            yield return new WaitForSeconds(_musicClips[clipIndex].length);
+            clipIndex = (clipIndex + 1) % _musicClips.Length;
         }
     }
 

78a9a926ea6e5da4f2cc0c67ba89ff33b119d1db
Michael Bråten <braaten.michael@outlook.com>
2017-11-04 18:02:36 +0100

    Can now steal abilities from the players you kill, if you don't have the ability yourself

--- BunnyGame/Assets/Scripts/Player/PlayerAbilityManager.cs
+++ BunnyGame/Assets/Scripts/Player/PlayerAbilityManager.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.Networking;
 
@@ -22,20 +23,81 @@ public class PlayerAbilityManager : NetworkBehaviour {
 
     // Called when the player kills another player
     [ClientRpc]
-    public void RpcStealAbility(string[] abilitynames, int id) {
-        Debug.Log("recieved abilities");
-        if (id != GetComponent<PlayerInformation>().playerControllerId)
+    public void RpcStealAbility(string[] theirAbilities, int id) {
+        if (id != GetComponent<PlayerInformation>().playerControllerId || !isLocalPlayer)
             return;
 
-        Debug.Log("*Stealing Ability*");
+
+        List<string> yourAbilities = new List<string>();
+        foreach (SpecialAbility ability in abilities)
+            yourAbilities.Add(ability.abilityName);
+
+        string[] newAbilities = theirAbilities.Except(yourAbilities).ToArray<string>();
+        
+        // If the player you killed had abilities you didn't have
+        if(newAbilities.Length > 0) {
+            int index = Random.Range(0, newAbilities.Length);
+            stealNewAbility(newAbilities[index]);
+        } else { // If you have all the abilities the other player had
+            int index = Random.Range(0, theirAbilities.Length);
+            upgradeExistingAbility(newAbilities[index]);
+        }
     }
+
+
+    private void stealNewAbility(string abilityName) {
+        SpecialAbility sa;
+        switch (abilityName) {
+            case "Sprint":
+                sa = gameObject.AddComponent<Sprint>();
+                ((Sprint)sa).init(35, 1);
+                break;
+            case "Stealth":
+                sa = gameObject.AddComponent<Stealth>();
+                ((Stealth)sa).init(1, 0);
+                break;
+            case "DustStorm":
+                sa = gameObject.AddComponent<DustStorm>();
+                ((DustStorm)sa).init();
+                break;
+            case "SuperJump":
+                sa = gameObject.AddComponent<SuperJump>();
+                ((SuperJump)sa).init(5);
+                break;
+            case "DustTornado":
+                sa = gameObject.AddComponent<DustTornadoAbility>();
+                ((DustTornadoAbility)sa).init();
+                break;
+            case "GrenadePoop":
+                sa = gameObject.AddComponent<GrenadePoop>();
+                ((GrenadePoop)sa).init();
+                break;
+            default:
+                Debug.Log("Ability does not exist: \"" + abilityName + "\" (PlayerAbilityManager.cs:stealNewAbility())");
+                return;
+        }
+        abilities.Add(sa);
+    }
+
+    private void upgradeExistingAbility(string abilityName) {
+        switch (abilityName) {
+            case "Sprint": break;
+            case "Stealth": break;
+            case "DustStorm": break;
+            case "SuperJump": break;
+            case "DustTornado": break;
+            case "GrenadePoop": break;
+        }
+    }
+
+
 
     [Command]
     public void CmdSendAbilitiesToKiller(int killerID) {
         List<string> abilitynames = new List<string>();
         foreach (SpecialAbility ability in abilities)
             abilitynames.Add(ability.abilityName);
+
         RpcStealAbility(abilitynames.ToArray(), killerID);
-        Debug.Log("sending abilities");
     }
 }

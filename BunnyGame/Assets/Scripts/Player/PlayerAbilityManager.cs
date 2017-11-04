0b6a59260947ce3a4e38248a15bdffeb717dfe73
Michael Bråten <braaten.michael@outlook.com>
2017-11-04 15:59:28 +0100

    .

--- BunnyGame/Assets/Scripts/Player/PlayerAbilityManager.cs
+++ BunnyGame/Assets/Scripts/Player/PlayerAbilityManager.cs
@@ -1,8 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
-public class PlayerAbilityManager : MonoBehaviour {
+public class PlayerAbilityManager : NetworkBehaviour {
     public List<SpecialAbility> abilities = new List<SpecialAbility>();
 
     // Use this for initialization
@@ -17,5 +18,24 @@ public class PlayerAbilityManager : MonoBehaviour {
                 StartCoroutine(abilities[i].useAbility());
             }
         }
+    }
+
+    // Called when the player kills another player
+    [ClientRpc]
+    public void RpcStealAbility(string[] abilitynames, int id) {
+        Debug.Log("recieved abilities");
+        if (id != GetComponent<PlayerInformation>().playerControllerId)
+            return;
+
+        Debug.Log("*Stealing Ability*");
+    }
+
+    [Command]
+    public void CmdSendAbilitiesToKiller(int killerID) {
+        List<string> abilitynames = new List<string>();
+        foreach (SpecialAbility ability in abilities)
+            abilitynames.Add(ability.abilityName);
+        RpcStealAbility(abilitynames.ToArray(), killerID);
+        Debug.Log("sending abilities");
     }
 }

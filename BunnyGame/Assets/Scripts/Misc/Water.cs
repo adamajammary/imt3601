823546bb149b8ce6aef67f01bb07909028765fe9
Michael Bråten <braaten.michael@outlook.com>
2017-11-16 11:41:00 +0100

    Fixed bug causing watereffect to not update properly while spectating

--- BunnyGame/Assets/Scripts/Misc/Water.cs
+++ BunnyGame/Assets/Scripts/Misc/Water.cs
@@ -16,31 +16,35 @@ public class Water : MonoBehaviour {
         mat.renderQueue = 3000;
     }
 
+    void Update() {
+        if(waterScreenEffect != null && GameObject.Find("Main Camera") != null)
+            waterScreenEffect.enabled = GameObject.Find("Main Camera").transform.position.y < _waterSurfaceHeight;
+    }
+
     void OnTriggerStay(Collider other) {
         if (other.tag == "PoopGrenade") return;
 
         if (other.tag == "Player") {
             other.GetComponent<PlayerController>().inWater = true;
-            waterScreenEffect.enabled = true;
+            //waterScreenEffect.enabled = true;
         }
         else if (other.tag == "bunnycamera") {
             if (other.transform.parent.tag == "Player") {
                 other.transform.parent.GetComponent<PlayerController>().inWater = true;
-                waterScreenEffect.enabled = true;
+                //waterScreenEffect.enabled = true;
             }
         }
-
     }
 
     void OnTriggerExit(Collider other) {
         if (other.tag == "Player") {
             other.GetComponent<PlayerController>().inWater = false;
-            waterScreenEffect.enabled = false;
+            //waterScreenEffect.enabled = false;
         }
         else if (other.tag == "bunnycamera") {
             if(other.transform.parent.tag == "Player") {
                 other.transform.parent.GetComponent<PlayerController>().inWater = false;
-                waterScreenEffect.enabled = false;
+                //waterScreenEffect.enabled = false;
             }
         }
     }

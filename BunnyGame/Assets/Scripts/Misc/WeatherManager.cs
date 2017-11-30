d8b0ac0a14fc05284efea51908cc7d8e93f19636
Michael Bråten <braaten.michael@outlook.com>
2017-11-30 14:53:02 +0100

    Weather is now networked and randomly selected

--- BunnyGame/Assets/Scripts/Misc/WeatherManager.cs
+++ BunnyGame/Assets/Scripts/Misc/WeatherManager.cs
@@ -1,8 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
-public class WeatherManager : MonoBehaviour {
+public class WeatherManager : NetworkBehaviour {
     public enum WeatherType {
         CLEAR,
         FOG,
@@ -31,24 +32,13 @@ public class WeatherManager : MonoBehaviour {
 
         _camera = GameObject.Find("Main Camera");
         _player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
+
+        if (this.isServer)
+            StartCoroutine(serverInit());
     }
 
 
     void Update() {
-        if (Input.GetKeyDown(KeyCode.F))
-            init(WeatherType.FOG);
-
-        if(this.weatherType == WeatherType.FOG) {
-            if (Input.GetKeyDown(KeyCode.O))
-                setFog(RenderSettings.fogDensity + 0.01f);
-            if (Input.GetKeyDown(KeyCode.L))
-                setFog(RenderSettings.fogDensity - 0.01f);
-        }
-
-
-        if (Input.GetKeyDown(KeyCode.R))
-            init(WeatherType.RAIN);
-
         if(this.weatherType == WeatherType.RAIN) {
             _rainEmitter.transform.position = _camera.transform.position + new Vector3(0, 50, 0);
             _rainSound.volume = (PlayerPrefs.GetFloat("Master Volume", 100) / 100f) * (PlayerPrefs.GetFloat("Effect Volume", 100) / 100f) * 0.25f;
@@ -56,9 +46,41 @@ public class WeatherManager : MonoBehaviour {
             _rainSound.pitch = _player.inWater ? 0.2f : 1f;
         }
 
+
+
+        // FOR TESTING ONLY BELOW:
+        if (Input.GetKeyDown(KeyCode.F))
+            init(WeatherType.FOG);
+        if (Input.GetKeyDown(KeyCode.R))
+            init(WeatherType.RAIN);
+
+        if (this.weatherType == WeatherType.FOG) {
+            if (Input.GetKeyDown(KeyCode.O))
+                setFog(RenderSettings.fogDensity + 0.01f);
+            if (Input.GetKeyDown(KeyCode.L))
+                setFog(RenderSettings.fogDensity - 0.01f);
+        }
+
     }
 
+    public IEnumerator serverInit() {
+        int playerCount = UnityEngine.Object.FindObjectOfType<NetworkPlayerSelect>().numPlayers;
+        while (playerCount != (GameObject.FindGameObjectsWithTag("Enemy").Length + 1)) //When this is true, all clients are connected and in the game scene
+            yield return 0;
+
+        Debug.Log("serverInit");
+        RpcInit((WeatherType)UnityEngine.Random.Range(0, 3));
+    }
+
+    [ClientRpc]
+    public void RpcInit(WeatherType w) {
+        Debug.Log("RpcInit");
+        init(w);
+    }
+
+
     public void init(WeatherType w) {
+        Debug.Log("init");
         weatherType = w;
         switch (w) {
             case WeatherType.CLEAR:
@@ -79,12 +101,11 @@ public class WeatherManager : MonoBehaviour {
 
 
     private void setFog(float density = 0.05f) {
-        Debug.Log("Setting fog");
         RenderSettings.fog = true;
         RenderSettings.fogDensity = density;
         RenderSettings.fogColor = new Color(0.9f, 0.9f, 0.9f);
         RenderSettings.fogMode = FogMode.Exponential;
-        GameObject.Find("Main Light").GetComponent<Light>().color = new Color(0.9f, 0.9f, 0.9f);
+        GameObject.Find("Main Light").GetComponent<Light>().color = new Color(0.8f, 0.8f, 0.8f);
         RenderSettings.skybox = fogSkyBox;
     }
 
